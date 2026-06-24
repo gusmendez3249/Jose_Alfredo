ecd53afd6ea6eae5f5d6994d2d63f2e7e5f4021d
gusmendez3249 <gustavoangelc2005@gmail.com>
2026-06-24 12:29:43 -0600

    first commit

---
+++ app/src/main/java/mx/utng/jose_alfredo/presentation/theme/Theme.kt
@@ -0,0 +1,17 @@
+package mx.utng.jose_alfredo.presentation.theme
+
+import androidx.compose.runtime.Composable
+import androidx.wear.compose.material3.MaterialTheme
+
+@Composable
+fun Jose_AlfredoTheme(
+    content: @Composable () -> Unit
+) {
+    /**
+     * Empty theme to customize for your app.
+     * See: https://developer.android.com/jetpack/compose/designsystems/custom
+     */
+    MaterialTheme(
+        content = content
+    )
+}
