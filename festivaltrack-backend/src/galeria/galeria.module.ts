41d4d1494572be93e633a92ff27e5264a3615fd5
SekiroK5 <arin01246@gmail.com>
2026-08-09 22:34:29 -0600

    feat(galeria): module for photo gallery and file upload

---
+++ festivaltrack-backend/src/galeria/galeria.module.ts
@@ -0,0 +1,11 @@
+import { Module } from '@nestjs/common';
+import { GaleriaController, UploadController } from './galeria.controller';
+import { GaleriaService } from './galeria.service';
+import { PrismaModule } from '../prisma/prisma.module';
+
+@Module({
+  imports: [PrismaModule],
+  controllers: [GaleriaController, UploadController],
+  providers: [GaleriaService]
+})
+export class GaleriaModule {}
